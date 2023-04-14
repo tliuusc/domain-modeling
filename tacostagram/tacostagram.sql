3dc521e6ae97792335b3407656ac03abbf8198e5
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-04-14 21:02:12 +0000

    Tacos

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,41 @@
+-- Drops existing tables, so we start fresh with each
+-- run of this script
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS users;
 
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_user_ID INTEGER,
+  image_URL TEXT,
+  time TEXT
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_ID INTEGER,
+  liker_user_ID INTEGER
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_ID INTEGER,
+  commenter_user_ID INTEGER,
+  comment TEXT
+);
+
+CREATE TABLE follows (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  follower_user_id INTEGER,
+  target_user_id INTEGER
+); 
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  location TEXT
+);
